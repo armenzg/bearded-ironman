f3a4c46978a4fa6245cf4a7bc1af805e294d1e66
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2012-11-13 13:51:54 -0500

    install_windows_sdk_8.0.nat small adjustments

--- WINNT6.2_x86-64/install_windows_sdk_8.0.bat
+++ WINNT6.2_x86-64/install_windows_sdk_8.0.bat
@@ -6,8 +6,8 @@ REM Files:  sdksetup.exe, asyncinfo.h
 REM
 REM [1] http://msdn.microsoft.com/en-us/windows/desktop/hh852363.aspx
 
-SET PACKAGES_URL="http://dev-stage01.build.mozilla.org/pub/mozilla.org/mozilla/libraries/win32/sdks"
-SET WGET="C:\mozilla-build\wget\wget.exe"
+SET PACKAGES_URL=http://dev-stage01.build.mozilla.org/pub/mozilla.org/mozilla/libraries/win32
+SET WGET=C:\mozilla-build\wget\wget.exe
 SET ASYNCINFO_PATH="C:\Program Files (x86)\Windows Kits\8.0\Include\winrt\asyncinfo.h"
 
 IF EXIST %ASYNCINFO_PATH% (
@@ -17,15 +17,15 @@ IF EXIST %ASYNCINFO_PATH% (
   echo "The Windows SDK 8.0 is not installed."
 )
 
-%WGET% -q "%PACKAGES_URL%/sdksetup.exe"
+%WGET% -q "%PACKAGES_URL%/sdks/sdksetup.exe"
 ECHO "Installing the SDK..."
 sdksetup.exe /q /norestart
 
 IF EXIST %ASYNCINFO_PATH% (
   ECHO "Replacing asyncinfo.h..."
   %WGET%  -q -Oasyncinfo.h "%PACKAGES_URL%/win8_64bit/asyncinfo.h"
-  rm %ASYNCINFO_PATH% 
-  cp asyncinfo.h %ASYNCINFO_PATH% 
+  rm %ASYNCINFO_PATH%
+  cp asyncinfo.h %ASYNCINFO_PATH%
   ECHO "asyncinfo.h has been replaced!"
 )
 
